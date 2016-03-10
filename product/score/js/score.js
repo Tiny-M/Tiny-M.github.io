1cffe87a3da223d9cce63599304e921a0da2fca3
Tiny-M <342743491@qq.com>
2016-03-10 18:39:41 +0800

    llama

--- product/score/js/score.js
+++ product/score/js/score.js
@@ -1,9 +1,19 @@
 var wH = window.innerHeight/2
 var s_2 = $(".s2")
+var s_6 = $(".s6")
 window.onscroll = function(){
 	if (scrollY>900-wH){
 		s_2.css("background-attachment","fixed")
 	}else{
 		s_2.css("background-attachment","scroll")
 	}
+
+	if(scrollY>3300-wH){
+		s_6.css("background-attachment","scroll")
+	}else{
+		s_6.css("background-attachment","fixed")
+	}
 }
+$(".dl_btn").bind("click",function(){
+	$(".qr_link").addClass("on")
+})
